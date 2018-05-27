714bf043a5becb53172774a44f78e8d9b82c414b
Cloud User <ec2-user@ip-172-31-31-105.us-west-2.compute.internal>
2018-05-27 17:04:11 +0000

    File tested and works on EC2 in both active and passive mode

--- ftp_server.sh
+++ ftp_server.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# This script will install vsftpd on this machine
+# This script will install vsftpd on this machine. It also creates a new test user - ftptest
+
 # Start of user inputs
+###############################################################################
+#PASSIVEMODE="yes"
+PASSIVEMODE="no"
+PASSIVESTARTPORT=45000
+PASSIVEENDPORT=45000
+# This is the public ip of the server
+SERVERIP=52.42.43.33
 FIREWALL="yes"
 #FIREWALL="no"
 USER1=ftptest
 PASSWORD1="redhat"
+###############################################################################
 # End of user inputs
 
 
@@ -20,7 +29,7 @@ else
 	echo "It will also create a test user ftptest"
 fi
 
-if yum list installed vsftpd
+if yum list installed vsftpd > /dev/null 2>&1
 then
 	systemctl -q is-active vsftpd && {
 		systemctl stop vsftpd
@@ -32,7 +41,7 @@ then
 fi
 
 echo "Installing $INSTALLPACKAGES.........."
-yum install -y $INSTALLPACKAGES
+yum install -y -q $INSTALLPACKAGES > /dev/null 2>&1
 echo "Done"
 
 systemctl start vsftpd
@@ -48,16 +57,31 @@ dirmessage_enable=YES
 xferlog_enable=YES
 connect_from_port_20=YES
 xferlog_std_format=YES
-listen=NO
-listen_ipv6=YES
+listen=YES
+listen_ipv6=NO
 pam_service_name=vsftpd
 userlist_enable=YES
-tcp_wrappers=YES
+
+# To use tcp_wrappers and /etc/hosts.allow and /etc/hosts.deny for IP/hostname access
+tcp_wrappers=NO
 userlist_file=/etc/vsftpd.userlist
 userlist_deny=NO
 chroot_local_user=YES
 allow_writeable_chroot=YES
 EOF
+
+if [[ $PASSIVEMODE == "yes" ]]
+then
+# Enable passive mode
+	echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_max_port=$PASSIVEENDPORT" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_min_port=$PASSIVESTARTPORT" >> /etc/vsftpd/vsftpd.conf
+	echo "# In case a server returns the internal IP on a PASV request" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_address=$SERVERIP" >> /etc/vsftpd/vsftpd.conf
+else
+	echo "pasv_enable=NO" >> /etc/vsftpd/vsftpd.conf
+fi
+
 
 setsebool -P ftpd_full_access=1
 
@@ -71,6 +95,11 @@ then
 	if systemctl -q is-active firewalld
 	then
 		firewall-cmd --permanent --add-service ftp
+		if [[ $PASSIVEMODE == "yes" ]]
+		then
+			firewall-cmd --permanent --remove-port=$PASSIVESTARTPORT-$PASSIVEENDPORT/tcp
+			firewall-cmd --permanent --add-port=$PASSIVESTARTPORT-$PASSIVEENDPORT/tcp
+		fi
 		firewall-cmd --reload
 		echo "ftp added to firewall"
 	else
@@ -78,5 +107,10 @@ then
 		echo "No changes made to firewall"
 	fi
 fi
+
+# Creating a file for testing
+dd if=/dev/zero of=/home/ftptest/xxx bs=1024 count=10
+echo `date` >> /home/ftptest/xxx
+chmod 777 /home/ftptest/xxx
 
 systemctl restart vsftpd
